3bf421ed7d9d747aa53defc3bbd0d440d46b6cc2
choiwinner <babyrain@naver.com>
2024-11-07 22:24:38 +0900

    :feet Chatbot 및 Image QA 개발 완료

--- Chatbot.py
+++ Chatbot.py
@@ -1,29 +1,89 @@
-from openai import OpenAI
+#실행 streamlit run Chatbot.py
+#python version = 3.10.12
+#langchain-google-genai =2.0.4
+#streamlit =1.38.0
+#gemini_api_key = 'AIzaSyCRNu3bAi56bwx_f34ehrBVfEZ7XuA7x7M'
 import streamlit as st
+import os
+
+
+#langchain, genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    gemini_api_key = st.text_input('Gemini_API_KEY를 입력하세요.', key="langchain_search_api_gemini", type="password")
+    "[Get an Gemini API key](https://ai.google.dev/)"
+    "[How to get Gemini API Key](https://luvris2.tistory.com/880)"
+
+    if (gemini_api_key[0:2] != 'AI') or (len(gemini_api_key) != 39):
+        st.warning('잘못된 key 입력', icon='⚠️')
+    else:
+        st.success('정상 key 입력', icon='👉')
+
+    if process :=st.button("Process"):
+        if (gemini_api_key[0:2] != 'AI') or (len(gemini_api_key) != 39):
+            st.error("잘못된 key 입력입니다. 다시 입력해 주세요.")
+            st.stop()
+
+    st.subheader('Gemini Model을 선택하세요.')
+    selected_model = st.sidebar.selectbox('Choose Gemini Model', ['gemini-1.5-flash', 'gemini-1.5-flash-latest','gemini-1.5-pro', 'gemini-1.5-pro-latest'], key='selected_model')
+    "gemini-1.5-flash : 분당 15회 요청 가능, 분당 100만 토큰 처리, 하루 1500회 요청 제한"
+    "gemini-1.5-pro : 분당 2회 요청 가능, 분당 32,000 토큰 처리, 하루 50회 요청 제한"
+    "[참고 자료 1](https://blog.naver.com/PostView.naver?blogId=itandtech&logNo=223624403146)"
+    "[참고 자료 2](https://ai.google.dev/gemini-api/docs/models/gemini?hl=ko)"
 
 st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+st.caption("🚀 A Streamlit chatbot powered by Gemini")
+
+#0. gemini api key Setting
+os.environ["GOOGLE_API_KEY"] = gemini_api_key
+
+#1. st.session_state 초기화
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state['messages'] = [] #st.session_state에 messages가 없으면 빈 리스트로 초기화
+    #2.'assistant' icon으로 write를 출력한다.
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
+st.chat_message("assistant").write("안녕하세요. 무엇을 도와드릴까요?")
 
-if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
+#2. 이전 대화 내용을 출력
+# st.session_state['messages']가 있고 길이가 0 이상이면 실행 
+if ("messages" in st.session_state) and (len(st.session_state['messages'])>0):
+    for role, message in st.session_state['messages']:  #st.session_state['messages']는 tuple 형태로 저장되어 있음.
+        st.chat_message(role).write(message)
+
+#3. query를 입력받는다.
+if query := st.chat_input("질문을 입력해주세요."): 
+    if not gemini_api_key:
+        st.warning("Gemini API Key를 입력해 주세요.")
         st.stop()
 
-    client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
-    st.chat_message("assistant").write(msg)
+    #4.'user' icon으로 query를 출력한다.
+    st.chat_message("user").write(query)
+    #5. query를 session_state 'user'에 append 한다.
+    st.session_state['messages'].append(('user',query))
+
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            chain = (
+                ChatPromptTemplate.from_template(
+                    """
+                    당신은 질문에 답하는 Chatbot으로 질문에 대해 자세하게 답변하고 답변의 참고 문헌이나 URL 정보도 알려주세요.
+                    [질문]
+                    {question}
+                    """
+                    ) 
+                    | ChatGoogleGenerativeAI(model=selected_model, temperature = 0) 
+                    | StrOutputParser()
+                    )
+            # chain 호출
+            response = chain.invoke({"question": query})
+            st.write(response)
+            st.session_state['messages'].append(('assistant',response))
+            
+# 참고 자료 
+# 1. https://www.youtube.com/watch?v=VtS8yF2ItgI
+# 2. https://wikidocs.net/book/14314
---
+++ pages/1_Image_Q&A.py
@@ -0,0 +1,96 @@
+#gemini_api_key = 'AIzaSyCRNu3bAi56bwx_f34ehrBVfEZ7XuA7x7M'
+import streamlit as st
+from PIL import Image
+import google.generativeai as genai
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+with st.sidebar:
+    gemini_api_key = st.text_input('Gemini_API_KEY를 입력하세요.', key="langchain_search_api_gemini", type="password")
+    "[Get an Gemini API key](https://ai.google.dev/)"
+    "[How to get Gemini API Key](https://luvris2.tistory.com/880)"
+
+    if (gemini_api_key[0:2] != 'AI') or (len(gemini_api_key) != 39):
+        st.warning('잘못된 key 입력', icon='⚠️')
+    else:
+        st.success('정상 key 입력', icon='👉')
+
+    if process :=st.button("Process"):
+        if (gemini_api_key[0:2] != 'AI') or (len(gemini_api_key) != 39):
+            st.error("잘못된 key 입력입니다. 다시 입력해 주세요.")
+            st.stop()
+
+    if data_clear :=st.button("대화 클리어"):
+        st.session_state['messages'] = [] #st.session_state[messages]를 초기화
+
+st.title("📝 Image Q&A with Gemini")
+
+#0. gemini api key Setting
+if not gemini_api_key:
+    st.warning("Gemini API Key를 입력해 주세요.")
+    st.stop()
+
+genai.configure(api_key=gemini_api_key)
+
+#1. st.session_state 초기화
+if "messages" not in st.session_state:
+    st.session_state['messages'] = [] #st.session_state에 messages가 없으면 빈 리스트로 초기화
+    #2.'assistant' icon으로 write를 출력한다.
+
+# 파일 업로더 위젯
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
+
+# 파일이 업로드되면 처리
+if uploaded_file is not None:
+
+    # 파일을 바이너리 데이터로 읽기
+    img_bytes = uploaded_file.read()
+
+    # PIL로 이미지를 열고 스트림릿 앱에 표시
+    #image = Image.open(uploaded_file).resize((300,300))
+    image = Image.open(uploaded_file)
+    #st.image(image, caption="Uploaded Image.", use_column_width=True)
+    st.image(image, caption="Uploaded Image.")
+    st.write("Image uploaded successfully!")
+else:
+    st.write("Please upload a JPG or PNG file.")
+
+# 파일을 지정한 경로에 저장 (임시 파일 경로)
+with open("temp_image.jpg", "wb") as f:
+    f.write(img_bytes)
+    
+# genai.upload_file 사용하여 llm에 파일 업로드
+uploaded_file_info = genai.upload_file(path="temp_image.jpg", display_name="uploaded_image")
+
+## Choose a Gemini API model.
+model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest")
+
+#1. st.session_state 초기화
+if "messages" not in st.session_state:
+    st.session_state['messages'] = [] #st.session_state에 messages가 없으면 빈 리스트로 초기화
+    #2.'assistant' icon으로 write를 출력한다.
+
+st.chat_message("assistant").write("안녕하세요. 무엇을 도와드릴까요?")
+
+#2. 이전 대화 내용을 출력
+# st.session_state['messages']가 있고 길이가 0 이상이면 실행 
+if ("messages" in st.session_state) and (len(st.session_state['messages'])>0):
+    for role, message in st.session_state['messages']:  #st.session_state['messages']는 tuple 형태로 저장되어 있음.
+        st.chat_message(role).write(message)
+
+#3. query를 입력받는다.
+if query := st.chat_input("질문을 입력해주세요."): 
+
+    #4.'user' icon으로 query를 출력한다.
+    st.chat_message("user").write(query)
+    #5. query를 session_state 'user'에 append 한다.
+    st.session_state['messages'].append(('user',query))
+
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            # llm에 사진에 대해서 물어 본다.
+            response = model.generate_content([uploaded_file_info, query])
+            st.write(response.text)
+            st.session_state['messages'].append(('assistant',response.text))
+
+
